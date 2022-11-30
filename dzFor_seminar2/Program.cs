76707edf4f5773152727e416a784b9dde18c4ee8
martT80 <maralkashkimbayevna@gmail.com>
2022-11-30 19:57:15 +0600

    dobavil tri

---
+++ dzFor_seminar2/Program.cs
@@ -0,0 +1,48 @@
+﻿// Семинар 2, Задача 10
+// Вывести вторую цифру введенного трехзначного числа
+
+ Console.WriteLine("Введите число от 100 до 999: ");
+ int num = Convert.ToInt32(Console.ReadLine());
+
+    if (num < 100 | num >= 1000) 
+    {  
+    Console.WriteLine("число не 3-х значное");
+    }
+    else {
+    Console.WriteLine(num / 10 % 10);  
+
+    }
+
+
+    
+// Семинар 2, Задача 13
+// Вывести третью цифру введенного числа, или сообщает что ее нет
+
+ Console.WriteLine("Введите число до 999: ");
+ int num1 = Convert.ToInt32(Console.ReadLine());
+ 
+    if (num1 < 100) {  
+       
+    Console.WriteLine("третьей цифры нет");
+    }
+    else {
+    Console.WriteLine(num1 % 10);  
+
+    }
+
+
+
+ // Семинар 2, Задача 15
+ // Являеться ли день недели выходным?
+
+ Console.WriteLine("Введите цифрами день недели: ");
+ int num1 = Convert.ToInt32(Console.ReadLine());
+ 
+    if (num1 != 6 && num1 != 7) {  
+ 
+    Console.WriteLine("день рабочий");
+    }
+    else {
+    Console.WriteLine("день выходной");  
+
+    }
