e168c6567b273dd7d2727ad14a20fe09ba1b9c5b
toMarathe80 <maralkashkimbayevna@gmail.com>
2022-12-21 12:45:45 +0600

    compl

---
+++ dzFre_seminar2/task10/Program.cs
@@ -0,0 +1,13 @@
+﻿// Семинар 2, Задача 10
+// Вывести вторую цифру введенного трехзначного числа
+
+ Console.WriteLine("Введите 3-х значное число: ");
+ int num = Convert.ToInt32(Console.ReadLine());
+
+    if (num < 100 || num >= 1000) {  
+    Console.WriteLine("число не 3-х значное");
+    }
+    
+    else {
+    Console.WriteLine(num / 10 % 10);  
+    }
