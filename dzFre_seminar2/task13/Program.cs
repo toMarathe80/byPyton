89c562f00c9f5eabdfa58c875dc2a05d8f58ff29
toMarathe80 <maralkashkimbayevna@gmail.com>
2022-12-21 12:46:52 +0600

    to_GB

---
+++ dzFre_seminar2/task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Семинар 2, Задача 13
+// Вывести третью цифру введенного числа, или сообщения что ее нет
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100) {
+    
+  Console.WriteLine("Здесь нет третьей цифры");
+ }
+ else {
+  while (number > 999) {
+    number = number / 10;
+  }
+  Console.WriteLine(number % 10);
+}
