7d45b089d5d49352b8dc28fef2c9d0d808a0ead0
toMarathe80 <maralkashkimbayevna@gmail.com>
2022-12-21 12:48:24 +0600

    end

---
+++ dzFre_seminar2/task15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Семинар 2, Задача 15
+// Являеться ли день недели выходным?
+
+ Console.WriteLine("Введите цифрами день недели: ");
+ int num = Convert.ToInt32(Console.ReadLine());
+ 
+    if (num > 8) {
+      Console.WriteLine("Расчет только на семь дней");
+    }
+    else if (num != 6 && num != 7) {  
+ 
+    Console.WriteLine("день рабочий");
+    }
+    else {
+    Console.WriteLine("день выходной");  
+
+    }
