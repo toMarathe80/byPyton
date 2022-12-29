71b402bc0e9790620c50b89c6b9f9b9e1c29697e
toMarathe80 <maralkashkimbayevna@gmail.com>
2022-12-29 12:22:16 +0600

    stepeni

---
+++ dzFre_seminar3/task23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Семинар 3, Задача 23
+// Вывести все числа в 3 степени до введенного в num
+
+ Console.WriteLine("Введите число: ");
+ int num1 = Convert.ToInt32(Console.ReadLine());
+ 
+ int count = 0;
+ Console.WriteLine();
+ while (count < num1) {
+
+    count += 1;
+    Console.WriteLine(count*count*count);
+    }
