483456eb7951f5d8f21a74905d23d197ddbb0ee5
toMarathe80 <maralkashkimbayevna@gmail.com>
2023-01-19 21:28:32 +0600

    shapka

---
+++ dz4_seminar4/task25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Семинар 4, задание 25
+// Возвести число А в натуральную степень В (3,5 -> 243)
+
+Console.Write("Введите число А: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+int step = 1;
+while (step <= degree) {
+    result = result * number; 
+    step += 1;   
+}
+Console.WriteLine(result);
