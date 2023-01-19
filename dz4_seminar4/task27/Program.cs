1735cff76fc9f508bffad89ed3e5d0aa66ad0006
toMarathe80 <maralkashkimbayevna@gmail.com>
2023-01-19 21:29:52 +0600

    dop_nazvan

---
+++ dz4_seminar4/task27/Program.cs
@@ -0,0 +1,11 @@
+﻿// Семинар 4, задание 27
+// Найти сумму цифр во введенном числе (452 -> 11, 82 -> 10, 9012 -> 12)
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int digit = 0;
+while(number > 0) {
+    digit = digit + number % 10;
+    number = number / 10;
+}
+Console.WriteLine("Сумма цифр числа: "+digit);
