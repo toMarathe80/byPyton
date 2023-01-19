669e808902956085b386918793353ca3803079ef
toMarathe80 <maralkashkimbayevna@gmail.com>
2023-01-19 21:31:40 +0600

    massiv8

---
+++ dz4_seminar4/task29/Program.cs
@@ -0,0 +1,16 @@
+﻿// Семинар 4, задание 29
+// Вывести на экран массив из 8-ми случайных элементов
+
+int [] array = new int [8];
+for(int i = 0; i < array.Length; i+= 1) {
+    array[i] = new Random().Next(0, 10);
+   }
+   Console.WriteLine("Массив из 8 случайных цифр: ");
+    for (int i = 0; i < array.Length; i+= 1) {
+        Console.WriteLine(array[i]);
+    }
+
+
+
+
+
