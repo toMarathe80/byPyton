6a9cf6126864f91729d353d48591e2588b7bf077
toMarathe80 <maralkashkimbayevna@gmail.com>
2023-01-27 20:56:39 +0600

    to git

---
+++ dz4_seminar6/task41/Program.cs
@@ -0,0 +1,17 @@
+﻿// Семинар 6, задание 41
+// Посчитать, сколько чисел больше 0 ввел пользователь
+
+Console.WriteLine("Введите 4 числа");
+int[] array = new int[4];
+for (int i = 0; i < 4; i += 1) {
+    Console.Write($"Номер {i+1}: ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine();
+    int count = 0;
+    foreach(int i in array) {
+        if(i > 0) {
+            count += 1;
+            }
+    }
+    Console.WriteLine("чисел больше 0: "+count);
