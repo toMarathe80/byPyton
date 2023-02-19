814e16afa5ffcdb1ae73f324a2694e691b704c79
toMarathe80 <maralkashkimbayevna@gmail.com>
2023-02-19 11:32:41 +0600

    recursia

---
+++ dz4_seminar9/task66/Program.cs
@@ -0,0 +1,25 @@
+﻿// Семинар 9, задание 66
+// Выполнить с помощью ресурсии вывод суммы чисел от М до N
+
+Console.Write("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int SumNumbers(int numberM, int NumberN)
+{
+  if (numberM < numberN){
+    if(numberM == numberN) return numberM;
+    else {
+    return numberM + SumNumbers(numberM+1, numberN);
+    }
+  }
+  else
+  {
+    if (numberN == numberM) return numberN;
+    else
+    {
+      return numberN + SumNumbers(numberN + 1, numberM);
+    }
+  }
+}
+Console.WriteLine(SumNumbers(numberM, numberN));
