c130f859c4be6fcb4bc2f59df1503763d9f43f9b
toMarathe80 <maralkashkimbayevna@gmail.com>
2023-02-19 11:36:56 +0600

    vStolbik

---
+++ dz4_seminar9/task64/Program.cs
@@ -0,0 +1,15 @@
+﻿// Семинар 9, задание 64
+// Выполнить с помощью ресурсии вывод всех чисел до N
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+void PrintNumbers(int number)
+{
+  if(number == 1) Console.WriteLine(number);
+  else 
+  {
+    Console.WriteLine(number);
+    PrintNumbers(number-1);
+  }
+}
+PrintNumbers(number);
