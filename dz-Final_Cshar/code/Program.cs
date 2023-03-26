4729dffffaffbff2bcd482b453d06ac3c2eaf121
toMarathe80 <maralkashkimbayevna@gmail.com>
2023-03-26 18:14:08 +0600

    чтот неработает

--- dz-Final_Cshar/code/Program.cs
+++ dz-Final_Cshar/code/Program.cs
@@ -5,5 +5,10 @@ string[]array1 = {"hello","2","world",":-)"};
 string[] array2 = new string[array1.Length-1];
 int index = 0;
 for(int i = 0; i < array1.Length; i += 1){
-
+    if (array1[i].Length <= 3){
+        array2[index] = array1[i];  
+        index += 1;        
+    }
 }
+Console.WriteLine(string.Join(" ",array2));
+
