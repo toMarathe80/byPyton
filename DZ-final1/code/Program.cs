a8ac7cf515d20256d28c8b2c455e09e302bf45dd
toMarathe80 <maralkashkimbayevna@gmail.com>
2023-03-26 17:40:17 +0600

    условие и основные переменные

---
+++ DZ-final1/code/Program.cs
@@ -0,0 +1,6 @@
+﻿// Из массива строк сформировать новый массив строк с эллементами 
+// равными или меньше 3 символа
+
+string[]array1 = {"hello","2","world",":-)"};
+string[] array2 = new string[array1.Length-1];
+int index = 0;
